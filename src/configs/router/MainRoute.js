12794c24f276a6d3fd67b74af102bf557a466caa
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-04-09 21:59:48 +0700

    fix mainRoute

--- src/configs/router/MainRoute.js
+++ src/configs/router/MainRoute.js
@@ -13,10 +13,10 @@ import ProductDetail from "../../pages/ProductDetail";
 import Profile from "../../pages/Profile";
 import SignUp from "../../pages/SignUp";
 import Login from "../../pages/Login";
-import Dashboard from '../../pages/Dashboard'
+import Dashboard from "../../pages/Dashboard";
 import ForgotPassword from "../../pages/ForgotPassword";
 
-import NewProduct from "../../pages/NewProduct"
+import NewProduct from "../../pages/NewProduct";
 
 function App() {
   return (
@@ -26,18 +26,13 @@ function App() {
         <PublicRoute path="/login" component={Login} />
         <PublicRoute path="/forgot-password" component={ForgotPassword} />
         <Route exact path="/" component={Home} />
-
-
-      
-        <Route path="/dashboard" component={Dashboard} />
         <PrivateRoute path="/payment-delivery" component={PaymentDelivery} />
         <PrivateRoute path="/chat" component={Chat} />
         <PrivateRoute path="/product-detail/:id" component={ProductDetail} />
-        <PrivateRoute path="/product-cust" component={ProductCustomer} />
+        <PrivateRoute path="/product-cust/" component={ProductCustomer} />
         <PrivateRoute path="/profile" component={Profile} />
         <PrivateRoute path="/history" component={History} />
-
-        {/* admin */}
+        <PrivateRoute path="/dashboard" component={Dashboard} />
         <PrivateRoute path="/new-product" component={NewProduct} />
       </Switch>
     </Router>
