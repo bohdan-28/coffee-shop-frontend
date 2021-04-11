8230b9b0fcc02cf93a62b4d054e91db2b67fde65
herzaparam <herzaparam@gmail.com>
2021-04-11 23:09:06 +0700

    get history and delete

--- src/parts/History/HistoryPart/HistoryPart.js
+++ src/parts/History/HistoryPart/HistoryPart.js
@@ -1,29 +1,93 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import style from "./history.module.css";
 import { withRouter } from "react-router-dom";
+import axios from 'axios'
 import Swal from 'sweetalert2'
 import CardHistory from '../CardHistory/CardHistory'
 import { handClick } from '../../../assets/images'
 
 const HistoryPart = (props) => {
-  // const [checked, setChecked] = useState(false)
-  // const [isSelected, setisSelected] = useState(false)
-  
 
-  // const deleteValidation = () => {
-  //   if (isSelected == false) {
-  //     setisSelected(true);
-  //   } 
-  //   return isSelected;
-  // }
+  const Url = process.env.REACT_APP_API_URL;
+  const Urlimg = process.env.REACT_APP_API_IMG;
+  const token = localStorage.getItem('token');
 
+  const [history, setHistory] = useState([])
+  const [toggle, setToggle] = useState([0])
+
+  useEffect(() => {
+    getHistory(Url, token)
+
+  }, []);
+
+  const getHistory = (Url, token) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`${Url}/orders/history/byid`, { headers: { authorization: `Bearer ${token}` } })
+        .then((res) => {
+          const newData = res.data.data.body
+          setHistory(
+            newData.map(d => {
+              return {
+                select: false,
+                id: d.id,
+                amount: d.amount,
+                image: d.image,
+                price: d.price,
+                product: d.product,
+                size: d.size
+              };
+            })
+          );
+          resolve(res.data.message)
+        }).catch((err) => {
+          reject(err)
+        })
+    })
+  };
+  const deleteHistoryById = () => {
+    Swal.fire({
+      title: "Are you sure you want to delete this?",
+      showDenyButton: true,
+      confirmButtonText: `Delete`,
+      confirmButtonColor: "#6a4029",
+      denyButtonText: "Cancel",
+      denyButtonColor: `#ffba33`,
+      focusDeny: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let arrayids = [];
+        history.forEach(d => {
+          if (d.select) {
+            arrayids.push(d.id)
+          }
+        });
+        console.log(arrayids);
+        axios
+          .delete(`${Url}/orders/history/byid/${arrayids}`, { headers: { authorization: `Bearer ${token}` } })
+          .then(data => {
+            window.location.reload();
+          })
+          .catch(err => alert(err));
+      } else {
+        Swal.fire({
+          title: "delete canceled",
+          text: "",
+          icon: "info",
+          confirmButtonText: "Ok",
+          confirmButtonColor: "#6a4029",
+        });
+      }
+    });
+  };
+    
+  console.log(history);
   return (
     <Fragment>
       <div className={style.history}>
         <div className="container">
           <div className={style['head-section']}>
             <div className="arr">
-              <i class={`${style.arrow} ${style.left}`}></i>
+              <i className={`${style.arrow} ${style.left}`}></i>
             </div>
             <h5>Order History</h5>
           </div>
@@ -35,17 +99,48 @@ const HistoryPart = (props) => {
             <p className={style.selectAll}>Select All</p> :
             <p className={style.selectAll}>Delete</p>
           } */}
+
           <div className={style.checkbox}>
-            <input type="checkbox" id="selectall" />
+            <input
+              type="checkbox"
+              id="selectall"
+              onChange={e => {
+                let checked = e.target.checked;
+                setHistory(
+                  history.map(d => {
+                    d.select = checked;
+                    return d;
+                  })
+                )
+              }} />
             <label htmlFor="selectall">Select All</label>
+            <button onClick={() => deleteHistoryById()}> Delete</ button>
           </div>
           <div className={style['body-section']}>
             <h6>Last Week</h6>
             <p>Select</p>
           </div>
           <div className="row flex-wrap">
-            <CardHistory />
-            <CardHistory />
+            {history.map((item, index) => {
+              return <CardHistory
+                image={`${Urlimg}${item.image}`}
+                name={item.product}
+                price={item.price}
+                size={item.size}
+                checked={item.select}
+                onChange={(e) => {
+                  let checked = e.target.checked;
+                  setHistory(
+                    history.map(data => {
+                      if (item.id === data.id) {
+                        data.select = checked;
+                      }
+                      return data;
+                    })
+                  );
+                }}
+                key={index} />
+            })}
           </div>
         </div>
       </div>
