48d35b89cf93a7ec0f6e23a4b177713ecb6ec538
nevalenaginda <nevalenaginda10@gmail.com>
2021-04-12 01:43:27 +0700

    add functional edit product to dev"

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -191,7 +191,7 @@ function Navbar() {
                   {user.image ? (
                     <img
                       className={Style["profile-img"]}
-                      src={`${ImgUrl}${user.image}`}
+                      src={`${ImgUrl}/${user.image}`}
                       alt="ImgUser"
                     />
                   ) : (
