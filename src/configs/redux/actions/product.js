20c808a492faeed15b47efb72a2eaa3704762eaf
nevalenaginda <nevalenaginda10@gmail.com>
2021-04-11 21:17:13 +0700

    add functional sort and pagination at product cust

--- src/configs/redux/actions/product.js
+++ src/configs/redux/actions/product.js
@@ -1,25 +1,40 @@
 import axios from "axios";
 import Swal from "sweetalert2";
+const getProductRequest = () => {
+  return { type: "GET_PRODUCT_REQUEST" };
+};
 
-export const getProduct = (data) => (dispatch) => {
+const getProductFailure = () => {
+  return { type: "GET_PRODUCT_FAILURE" };
+};
+
+export const getProduct = (data, page = 1, perPage = 4, sortBy, order) => (
+  dispatch
+) => {
   const Url = process.env.REACT_APP_API_URL;
   return new Promise((resolve, reject) => {
     if (data === "favourite") {
+      dispatch(getProductRequest);
       axios
-        .get(`${Url}/products/${data}`)
+        .get(
+          `${Url}/products/${data}?page=${page}&perPage=${perPage}&sortBy=${sortBy}&order=${order}`
+        )
         .then((res) => {
-          const product = res.data.data;
+          const product = res.data;
           dispatch({ type: "GET_PRODUCT", payload: product });
           resolve(res.data.data);
         })
         .catch((err) => {
+          dispatch(getProductFailure);
           reject(new Error(err.response.data.message));
         });
     } else {
       axios
-        .get(`${Url}/products?keyword=${data}`)
+        .get(
+          `${Url}/products?keyword=${data}&page=${page}&perPage=${perPage}&sortBy=${sortBy}&order=${order}`
+        )
         .then((res) => {
-          const product = res.data.data;
+          const product = res.data;
           dispatch({ type: "GET_PRODUCT", payload: product });
           resolve(res.data.data);
         })
