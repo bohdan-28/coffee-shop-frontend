2e9a54d399c4f8807a3f6a76dd170c55efa9a421
Herza <herzaparam@gmail.com>
2021-05-25 11:51:38 +0700

    Delete src directory

--- src/configs/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducer/index";
-import thunk from "redux-thunk";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
